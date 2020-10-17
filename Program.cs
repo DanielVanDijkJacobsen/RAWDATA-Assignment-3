ea9be97c5c4c69cd0f5cd7d04e21077f7c0d4f1d
DanielVanDijkJacobsen <danilar@ruc.dk>
2020-10-17 20:16:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+This program was developed by Christensen, Martin Smed; Jacobsen, Daniel van Dijk; Romby, Josephine; Varchev, Aleksander Iliyanov
+in relation to the RAWDATA course at Roskilde University.
+ */
+using System;
 
 namespace RAWDATA_Assignment_3
 {
