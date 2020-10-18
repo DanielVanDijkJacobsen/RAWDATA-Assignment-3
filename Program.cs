8973d366e73fb22cf7c29eab1a261a41dafc3b76
DanielVanDijkJacobsen <danilar@ruc.dk>
2020-10-18 20:46:39 +0200

    Copy from Henrik

--- Program.cs
+++ Program.cs
@@ -3,14 +3,47 @@ This program was developed by Christensen, Martin Smed; Jacobsen, Daniel van Dij
 in relation to the RAWDATA course at Roskilde University.
  */
 using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace RAWDATA_Assignment_3
 {
-    class Program
+    class ServerProgram
     {
         static void Main(string[] args)
         {
+            var server = new TcpListener(IPAddress.Loopback, 5000);
+            server.Start();
+            Console.WriteLine("Server started!");
 
+            while (true)
+            {
+                var client = server.AcceptTcpClient();
+                Console.WriteLine("Accepted client!");
+
+                var stream = client.GetStream();
+
+
+                var msg = Read(client, stream);
+
+                Console.WriteLine($"Message from client {msg}");
+
+                var data = Encoding.UTF8.GetBytes(msg.ToUpper());
+
+                stream.Write(data);
+
+            }
+        }
+
+        private static string Read(TcpClient client, NetworkStream stream)
+        {
+            byte[] data = new byte[client.ReceiveBufferSize];
+
+            var cnt = stream.Read(data);
+
+            var msg = Encoding.UTF8.GetString(data, 0, cnt);
+            return msg;
         }
     }
 }
